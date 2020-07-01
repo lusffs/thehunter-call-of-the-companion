db7989c86ce8061ec26bedfe0675fc47ac2a3aa0
Conrad Ohlsson <lusffs@gmail.com>
2020-07-01 15:35:48 +0200

    Add unformatted data

--- src/_data/animals.js
+++ src/_data/animals.js
@@ -135,3 +135,76 @@ export default [
     },
   },
 ];
+/*
+
+Trophy rating	| Weight [kg] |	Weight [lbs]	| Max level | Name | Class
+
+Cuatro Colinas
+7	7	15	3		European Hare 1
+87	33	73	3		Roe Deer 3
+32	219	482	5		Wild Boar 4
+250	67	148	5		Ronda Ibex 4
+127	100	219	5		Gredos Ibex 4
+109	107	235	5		Beceite Ibex 4
+103	82	181	5		Spanish Ibex 4
+205	58	128	5		Iberian Mouflon 4
+39	48	105	9		Iberian Wolf 5
+248	232	511	9		Red Deer 6
+
+Yukon Valley
+7	1	2	3		Harlequin Duck 1
+29	14	31	9		Red Fox 2
+39	75	165	9		Grey Wolf 5
+400	186	410	5		Caribou 6
+67	628	1385	9		Grizzly Bear 7
+291	590	1301	5		Moose 8
+151	1115	2458	5		Plains Bison 9
+
+Parque Fernando
+7	0	1	3		Cinnamon Teal 1
+351	69	153	5		Axis Deer 3
+28	47	104	5		Blackbuck 3
+39	97	215	9		Puma 5
+400	197	434	5		Mule Deer 6
+248	232	511	9		Red Deer 6
+192	1177	2595	9		Water Buffalo 9
+
+Vurhogna Savanna
+5	5	12	3		Scrub Hare 1
+57	13	29	9		Jackal 2
+111	41	90	5		Springbok 3
+270	98	216	5		Lesser Kudu 4
+58	140	308	5		Warthog 4
+38	272	600	5		Blue Wildebeest 6
+375	238	525	5		Gemsbok 8
+49	257	565	9		Lion 9
+203	895	1973	9		Cape Buffalo 9
+
+Medved Taiga
+14	16	35	3		Musk Deer 2
+28	41	91	9		Eurasian Lynx 3
+32	219	482	5		Wild Boar 4
+495	180	396	3		Reindeer 6
+28	445	981	9		Brown Bear 7
+291	590	1301	5		Moose 8
+
+Hirschfelden
+9	9	19	5		Canada Goose 1
+29	14	31	9		Red Fox 2
+87	33	73	3		Roe Deer 3
+172	96	211	5		Fallow Deer 4
+32	219	482	5		Wild Boar 4
+248	232	511	9		Red Deer 6
+221	868	1914	5		European Bison 9
+
+Layton Lakes
+5	2	4	3		Mallard 1
+6	6	14	3		Jackrabbit 1
+248	26	57	9		Coyote 2
+207	90	197	5		Blacktail Deer 4
+144	96	211	3		Whitetail Deer 4
+23	265	584	9		Black Bear 7
+338	479	1057	5		Roosevelt Elk 8
+291	590	1301	5		Moose 8
+
+*/
