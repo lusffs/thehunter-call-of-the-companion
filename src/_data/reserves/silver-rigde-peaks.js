16dc9c1a7ae3f7fe388cfdebe710fb615a300e4d
Conrad Ohlsson <lusffs@gmail.com>
2020-07-06 19:15:58 +0200

    Add in game timer to application

--- src/_data/reserves/silver-rigde-peaks.js
+++ src/_data/reserves/silver-rigde-peaks.js
@@ -1,4 +1,117 @@
+const status = {
+  resting: "Resting",
+  feeding: "Feeding",
+  drinking: "Drinking",
+};
+
 export default {
   name: "Silver Ridge Peaks",
   // animals: ["001", "002", "003", "004", "005", "006", "007", "008", "009"],
+  needZones: [
+    {
+      name: "Turkey",
+      times: [
+        {
+          from: 0,
+          to: 5,
+          status: status.resting,
+        },
+        {
+          from: 5,
+          to: 15,
+          status: status.feeding,
+        },
+        {
+          from: 15,
+          to: 16,
+          status: status.drinking,
+        },
+        {
+          from: 16,
+          to: 21,
+          status: status.feeding,
+        },
+        {
+          from: 21,
+          to: 0,
+          status: status.resting,
+        },
+      ],
+    },
+  ],
 };
+
+/* 
+Turkey	
+  00:00 - 05:00	Resting
+	05:00 - 15:00	Feeding
+	15:00 - 16:00	Drinking
+	16:00 - 21:00	Feeding
+  21:00 - 00:00	Resting
+  
+Pronghorn	
+  00:00 - 05:00	Resting
+  05:00 - 08:00 Feeding
+  08:00 - 10:00	Drinking
+	10:00 - 19:00	Feeding
+  19:00 - 21:00	Drinking
+  21:00 - 00:00 Resting
+				
+Mountain Goat	
+  00:00 - 03:00	Feeding
+	03:00 - 05:00	Resting
+	05:00 - 09:00	Feeding
+	09:00 - 11:00	Drinking
+	11:00 - 14:00	Feeding
+  13:00 - 16:00	Resting
+  16:00 - 21:30 Feeding
+  21:00 - 22:30 Drinking
+  22:00 - 00:00 Resting
+
+Bighorn Sheep	
+  00:00 - 04:00 Resting
+	04:00 - 06:00	Feeding
+	06:00 - 08:00	Drinking
+	08:00 - 21:00	Feeding
+  21:00 - 00:00	Resting
+
+Mountain Lion	
+  00:00 - 06:00	Feeding
+	06:00 - 08:00	Drinking
+	08:00 - 17:00	Resting
+  17:00 - 18:00 Drinking
+  18:00 - 00:00	Feeding
+
+Mule Deer	
+  00:00 - 05:00	Feeding
+	05:00 - 09:00	Resting
+	09:00 - 10:00	Drinking
+	10:00 - 14:00	Feeding
+  14:00 - 18:00	Resting
+  18:00 - 23:00 Feeding
+  23:00 - 0:00 Drinking
+		
+Black Bear	
+  00:00 - 05:00	Resting
+	05:00 - 11:00	Feeding
+	11:00 - 12:00	Drinking
+	12:00 - 15:00	Resting
+	15:00 - 20:00	Drinking
+  20:00 - 21:00	Drinking
+  21:00 - 00:00 Resting
+
+Rocky Mountain Elk	
+  00:00 - 04:00	Resting
+	04:00 - 05:00	Drinking
+	05:00 - 17:00	Feeding
+	17:00 - 18:00	Drinking
+  18:00 - 00:00	Feeding
+  
+Plains Bison		
+  00:00 - 03:00 Resting  
+  03:00 - 10:00	Feeding
+	10:00 - 11:00	Drinking
+	11:00 - 16:00	Resting
+	16:00 - 17:00	Drinking
+  17:00 - 00:00	Feeding
+  */
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Link as RouterLink } from "react-router-dom";
 import Box from "@material-ui/core/Box";
@@ -7,13 +7,69 @@ import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import backgroundImage from "assets/images/background2.jpg";
 import CrosshairIcon from "components/icons/Crosshair";
+import TextField from "@material-ui/core/TextField";
+
+const SERVER_SECOND_VALUE = 3600 / 920; // 1h in game = 15:20 minutes realtime => 3.913 seconds per 1 real life second
 
 export default function Start() {
   const classes = useStyles();
+  const [inGameTime, setInGameTime] = useState("07:00");
+  const [clientSyncTime, setClientSyncTime] = useState();
+  const [worldTime, setWorldTime] = useState();
+  const [serverClock, setServerClock] = useState("");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        const msSinceStart = new Date().getTime() - clientSyncTime;
+
+        const serverTime = new Date(worldTime);
+        serverTime.setMilliseconds(
+          serverTime.getMilliseconds() + msSinceStart * SERVER_SECOND_VALUE
+        );
+
+        setServerClock(
+          `${
+            serverTime.getHours() < 10
+              ? `0${serverTime.getHours()}`
+              : serverTime.getHours()
+          }:${
+            serverTime.getMinutes() < 10
+              ? `0${serverTime.getMinutes()}`
+              : serverTime.getMinutes()
+          }`
+        );
+      }, 250);
+    } else if (!isActive) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive]);
+
+  const handleTimerClick = () => {
+    setWorldTime(
+      new Date().setHours(
+        parseInt(inGameTime.split(":")[0]),
+        parseInt(inGameTime.split(":")[1]),
+        0,
+        0
+      )
+    );
+    setClientSyncTime(new Date().getTime());
+    setIsActive(!isActive);
+  };
+
+  const handleTimerChange = (e) => {
+    setInGameTime(e.target.value);
+  };
+
   return (
     <>
       <Box className={classes.hero}>
@@ -45,6 +101,31 @@ export default function Start() {
               </Box>
             </Paper>
           </Grid>
+          <Grid item>
+            <Paper>
+              <Box my={3} p={3}>
+                <form className={classes.container} noValidate>
+                  <TextField
+                    id="time"
+                    label="In game time"
+                    type="text"
+                    defaultValue={inGameTime}
+                    className={classes.textField}
+                    onChange={handleTimerChange}
+                    disabled={isActive}
+                  />
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    onClick={handleTimerClick}
+                  >
+                    {isActive ? "Stop time sync" : "Start time sync"}
+                  </Button>
+                  {serverClock && <h1>{serverClock}</h1>}
+                </form>
+              </Box>
+            </Paper>
+          </Grid>
         </Grid>
       </Container>
     </>
@@ -67,3 +148,18 @@ const useStyles = makeStyles(() => ({
     textAlign: "center",
   },
 }));
+/**
+ * 
+ * function useStickyState(defaultValue, key) {
+  const [value, setValue] = React.useState(() => {
+    const stickyValue = window.localStorage.getItem(key);
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
+ */
